6c860cbc104739a0f72be7b53852aa3ebf76318a
i2m fujikawa <h.fujikawa@image2measure.com>
2017-10-15 19:31:16 +0900

    add converting from opencv to pillow

--- depth_cnv.py
+++ depth_cnv.py
@@ -5,11 +5,24 @@ https://stackoverflow.com/questions/32323922/how-to-convert-a-24-color-bmp-image
 @author: hfuji
 """
 
+import cv2
+import numpy as np
 from PIL import Image
 
-iname = 'bin1.png'
-oname = 'test.png'
+colors =[(255,0,0), (255,127,0), (255,255,0), (127,2550,0),
+         (0,255,0),(0,255,127), (0,255,2255), (0,127,255),
+         (0,0,255), (127,0,255),(255,0,255), (255,0,127)]
 
-img = Image.open(iname)
-newimg = img.convert(mode='P', colors=8)
-newimg.save(oname)
+height = 300
+width = 300
+img = np.zeros([height, width, 3], dtype=np.uint8)
+row_size = height / len(colors)
+for i in range(len(colors)):
+    colorRGB = colors[i]
+    colorBGR = (colorRGB[2], colorRGB[1], colorRGB[0])
+    img[i*row_size:i*row_size+row_size, :] = colorBGR
+
+tempRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img24Color = Image.fromarray(tempRGB)
+img8Color = img24Color.convert(mode='P', colors=8)
+img8Color.save('img8Color.png')
