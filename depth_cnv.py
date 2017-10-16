5ea077c2925128cd30df33ad524b7fbc6f1d1d17
i2m fujikawa <h.fujikawa@image2measure.com>
2017-10-16 21:56:56 +0900

    convert by adaptive pallete

--- depth_cnv.py
+++ depth_cnv.py
@@ -2,10 +2,11 @@
 """
 Created on Sat Oct 14 07:46:17 2017
 https://stackoverflow.com/questions/32323922/how-to-convert-a-24-color-bmp-image-to-16-color-bmp-in-python
+https://stackoverflow.com/questions/1065945/how-to-reduce-color-palette-with-pil
 @author: hfuji
 """
 
-import cv2
+#import cv2
 import numpy as np
 from PIL import Image
 
@@ -18,11 +19,8 @@ width = 300
 img = np.zeros([height, width, 3], dtype=np.uint8)
 row_size = height / len(colors)
 for i in range(len(colors)):
-    colorRGB = colors[i]
-    colorBGR = (colorRGB[2], colorRGB[1], colorRGB[0])
-    img[i*row_size:i*row_size+row_size, :] = colorBGR
+    img[i*row_size:i*row_size+row_size, :] = colors[i]
 
-tempRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img24Color = Image.fromarray(tempRGB)
-img8Color = img24Color.convert(mode='P', colors=8)
+img24Color = Image.fromarray(img)
+img8Color = img24Color.convert('P', palette=Image.ADAPTIVE, colors=8)
 img8Color.save('img8Color.png')
